52b27af60395b3acb5b78bebe46544e07ab5689f
rotciv <victorfage@gmail.com>
2020-07-21 18:06:14 +0200

    fix env var

--- src/init.js
+++ src/init.js
@@ -7,18 +7,17 @@ module.exports = async () => {
   console.log('Start Log:', Date().toString());
 
   process.configDefault = require('../configDefault.js');
-  process.web3 = new Web3(new Web3.providers.HttpProvider(process.env.URL_NODE_ETHEREUM));
-  console.log(process.configDefault);
+  process.web3 = new Web3(new Web3.providers.HttpProvider(process.configDefault.URL_NODE_ETHEREUM));
   process.contracts = await require('./contracts.js')();
 
   // fix process pks split
-  // process.configDefault.BOT_PKS = splitAddresses(process.env.BOT_PKS);
-  process.walletManager = new WalletManager([ process.env.BOT_PKS ]);
+  // process.configDefault.BOT_PKS = splitAddresses(process.configDefault.BOT_PKS);
+  process.walletManager = new WalletManager([ process.configDefault.BOT_PKS ]);
   process.callManager = new CallManager();
 
-  process.takeOn = process.env.TAKE;
-  process.claimOn = process.env.CLAIM;
-  //process.reporterOn = process.env.REPORTER;
+  process.takeOn = process.configDefault.TAKE;
+  process.claimOn = process.configDefault.CLAIM;
+  //process.reporterOn = process.configDefault.REPORTER;
 };
 
 function splitAddresses(addresses) {
